4ddeca31c2ace303d60c97edcd0ac9cd659abfa3
VivienS5 <sobra.vivien@gmail.com>
2024-10-22 10:36:18 +0200

    modif models books

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,7 +1,13 @@
-namespace Aurible.Models
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aurible.Models 
 {
     public class Book
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int idBook { get; set; }
         public string? title { get; set; }
         public string? resume { get; set; }
